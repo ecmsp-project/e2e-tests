cfbd53325dd65331a2ffaef90c37878b518c0001
Ariel <arie.michalik@gmail.com>
2025-10-07 13:34:45 +0200

    refactor order-related DTOs to include variantId and enhance response structures

--- src/main/java/com/ecmsp/e2e/dto/order/GetOrderResponseDto.java
+++ src/main/java/com/ecmsp/e2e/dto/order/GetOrderResponseDto.java
@@ -3,10 +3,9 @@ package com.ecmsp.e2e.dto.order;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record Order(
+public record GetOrderResponseDto(
         String orderId,
-        String clientId,
         String orderStatus,
         LocalDateTime date,
-        List<OrderItem> items) {
+        List<GetOrderItemDetailsDto> items) {
 }
